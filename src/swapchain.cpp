e91ac6953797ce19b959ced979ad723b82362d41
Retro52 <orlenko.anto@gmail.com>
2026-02-06 20:03:13 +0100

    fix Swapchain validation error on windows due to incorrect usage flags
    Fixes validation error:
    vkCreateFramebuffer(): pCreateInfo->pAttachments[0] expected usage (VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT) conflicts with the image's flags (VK_IMAGE_USAGE_STORAGE_BIT).
    The Vulkan spec states: If flags does not include VK_FRAMEBUFFER_CREATE_IMAGELESS_BIT, each element of pAttachments that is used as a color attachment or resolve attachment by renderPass must have been created with a usage value including VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT (https://vulkan.lunarg.com/doc/view/1.4.328.1/windows/antora/spec/latest/chapters/renderpass.html#VUID-VkFramebufferCreateInfo-pAttachments-00877)

--- src/swapchain.cpp
+++ src/swapchain.cpp
@@ -112,7 +112,7 @@ void createSwapchain(Swapchain& result, VkPhysicalDevice physicalDevice, VkDevic
 	createInfo.imageExtent.width = width;
 	createInfo.imageExtent.height = height;
 	createInfo.imageArrayLayers = 1;
-	createInfo.imageUsage = VK_IMAGE_USAGE_STORAGE_BIT;
+	createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_STORAGE_BIT;
 	createInfo.queueFamilyIndexCount = 1;
 	createInfo.pQueueFamilyIndices = &familyIndex;
 	createInfo.preTransform = VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR;
